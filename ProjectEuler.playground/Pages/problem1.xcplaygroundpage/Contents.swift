d2b13e8a8502941eb60fe8ae75a76aea6ab04cce
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2022-03-20 00:29:55 +0300

    done

---
+++ ProjectEuler.playground/Pages/problem1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all the multiples of 3 or 5 below 1000.
+
+//solution 1
+let num = 1000
+var sum = 0
+for i in 0..<num {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i
+        }
+}
+print(sum)
+
+//solution 2
+ func sumOfMultiples(with multiple1: Int, with multiple2: Int, below: Int) -> Int{
+    var sum = 0
+    for i in 1..<below {
+        if i % multiple1 == 0 || i % multiple2 == 0 {
+            sum += i
+        }
+    }
+    return sum
+}
+
+print(sumOfMultiples(with: 3, with: 5, below: 1000))
+
+
